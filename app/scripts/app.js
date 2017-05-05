4a74a79b46e64c2a1115d5ff57f270697de724cf
Daniela Sannino <danielasannino@daniela-sanninos-macbook.local>
2017-05-04 20:35:04 -0400

    added collection state & updated nav tag. fixed landing state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,28 @@
 (function() {
-     function config($stateProvider, $locationProvider) {
-         $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
-         
-         $stateProvider
-             .state('landing', {
-                 url: '/',
-                 controller: 'LandingCtrl as landing',
-                 templateUrl: '/templates/landing.html'
-             });
-     }
- 
-     angular
-         .module('blocJams', ['ui.router'])
-         .config(config);
- })();
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        $stateProvider
+            .state('landing', {
+            url: '/',
+            controller: 'LandingCtrl as landing',
+            templateUrl: '/templates/landing.html'
+        });
+        .state('album', {
+            url: '/',
+            templateUrl: '/templates/album.html'
+        })
+            .state('collection', {
+            url: '/',
+            templateUrl: '/templates/collection.html'
+        });
+    }
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();

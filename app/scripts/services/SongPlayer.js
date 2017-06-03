37c12a3a5ac9ce86b17940231c0ce53b393a3712
Daniela Sannino <danielasannino@daniela-sanninos-macbook.local>
2017-06-03 14:09:42 -0400

    Checkpoint 10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-   function SongPlayer(Fixtures) {
+   function SongPlayer($rootScope,Fixtures) {
         var SongPlayer = {};
        
        /**
@@ -36,6 +36,12 @@
         		formats: ['mp3'],
         		preload: true
         	});
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+          });
 
         	SongPlayer.currentSong = song;
         };
@@ -79,7 +85,11 @@
         */
        
         SongPlayer.currentSong = null;
-        
+         /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
        
        /**
         * @method SongPlayer.play
@@ -145,11 +155,22 @@
                playSong(song);
            }
        };
+       
+            /**
+     * @function setCurrentTime
+     * @desc Set current time (in seconds) of currently playing song
+     * @param {Number} time
+     */
+     SongPlayer.setCurrentTime = function(time) {
+         if (currentBuzzObject) {
+             currentBuzzObject.setTime(time);
+         }
+     };
 
         return SongPlayer;
     }
  
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
